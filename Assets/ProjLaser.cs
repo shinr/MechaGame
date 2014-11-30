eae47643c723cf6bb133b7cd37af4d2434f11485
shinr <jukka.lansineva@student.oulu.fi>
2014-11-30 18:44:27 +0200

    ProjLaser start
    basic stuff

--- Assets/ProjLaser.cs
+++ Assets/ProjLaser.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class ProjLaser : MonoBehaviour {
-
+	Vector3 speed;
+	float speedMultiplier = 40.0f;
+	void OnCollisionEnter(Collider collision) {
+		Destroy (this);
+	}
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,6 @@ public class ProjLaser : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.rigidbody.AddForce (speed * speedMultiplier);
 	}
 }
